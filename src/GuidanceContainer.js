96f7a9b76f9965e78ac4c6bf38501415277c68d7
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-05-11 13:17:00 -0400

    add ? to Priority thead

--- src/GuidanceContainer.js
+++ src/GuidanceContainer.js
@@ -15,7 +15,7 @@ export default class GuidanceContainer extends React.Component {
                             <td>Image</td>
                             <td>Description</td>
                             <td>Stars</td>
-                            <td>Priority</td>
+                            <td>Priority?</td>
                         </tr>
                     </thead>
                     <tbody>
@@ -26,12 +26,9 @@ export default class GuidanceContainer extends React.Component {
                                             What warning did you get and how did it help you?
                         */}
                         {
-                            wellnessFactors.map((factor, index) => <GuidanceItem 
-                            key={index} 
-                            factor={factor}
-                            />)
+                            wellnessFactors.map((factor, index) => <GuidanceItem key={index} factor={factor}/>)
                         }
-                           
+         
                     </tbody>
                 </table>
             </div>
@@ -47,3 +44,14 @@ export default class GuidanceContainer extends React.Component {
         )
     }
 }
+
+
+// {
+//     id: 1,
+//     title: 'Physical',
+//     image: 'https://thewellatsacstate.com/cache/ce-image/dimensions-of-wellness/uploads/images/7_dimensions/7D_Icon_Physical_500x500ppi_500_500_bor3_0fa195_all_250.png',
+//     color: 'red',
+//     description: 'Taking care of one\'s body.',
+//     stars: 4,
+//     isPriority: true,
+// }
