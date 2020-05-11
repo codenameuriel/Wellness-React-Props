e0087f3ad54f6e6762d6ece4facea00220d55771
Ramzy Badawy <Ramzy.Badawy@gmail.com>
2020-05-11 13:08:26 -0400

    commit

--- src/Wheel.js
+++ src/Wheel.js
@@ -5,14 +5,25 @@ import { Pie } from './styles';
 
 
 export default class Wheel extends React.Component {
+    
+    generateWellnessItem = () => {
+        return wellnessFactors.map((factor,index) => 
+            <WellnessItem 
+            key = {index}
+            id = {factor.id}
+            color = {factor.color}
+            title = {factor.title}
+            isPriority = {factor.isPriority}
+            />
+        )
+    }
+
+
     render(){
         return (
             <Pie size={300}>
-                {/** TODO: map over your wellness factors and create a WellnessItem for each. 
-                 *         check the WellnessItem component to see what it's expecting for props
-                 *         Hint: Use your ES6 techniques
-                 */}
+                {this.generateWellnessItem()}
             </Pie>
         )
     }
-}
+} 
