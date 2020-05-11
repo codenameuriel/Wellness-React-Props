78dba380f49aad78d2fe176d9c18e4863fd77216
Leon Jamison <leonjamison@icloud.com>
2020-05-11 11:57:18 -0400

    working on 1st deliverable

--- src/Wheel.js
+++ src/Wheel.js
@@ -8,6 +8,14 @@ export default class Wheel extends React.Component {
     render(){
         return (
             <Pie size={300}>
+                <wellnessFactors.map ( WellnessItem (title, image, color, description, stars, isPriority) => {
+                    return(
+
+                        <div className='wellness-item'>
+                            <h1>{}</h1>
+                        </div>
+                    )
+                }) />
                 {/** TODO: map over your wellness factors and create a WellnessItem for each. 
                  *         check the WellnessItem component to see what it's expecting for props
                  *         Hint: Use your ES6 techniques
