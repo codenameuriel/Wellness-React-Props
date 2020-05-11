d62274ce8b632a404f8f9bcce27aeef08b8b9a9f
Josephscha <Josephshincha@gmail.com>
2020-05-10 22:54:33 -0400

    Guidance items/Container finished. Unable to assign key to children

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Wheel from './Wheel.js'
-// import GuidanceContainer from './GuidanceContainer';
+import GuidanceContainer from './GuidanceContainer';
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <h1> Welcome to the Wellness Center</h1>
       <Wheel />
       <hr />
-      {/* <GuidanceContainer /> */}
+      <GuidanceContainer />
     </div>
   );
 }
--- src/GuidanceItem.js
+++ src/GuidanceItem.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { wellnessFactors } from './data';
 
-
-
-const GuidanceItem = () => {
+const GuidanceItem = (props) => {
     return (
         <tr>
             <td>{props.factor.title}</td>
             <td><img src={props.factor.image} alt={props.factor.title}/></td>
-            <td>{/** TODO: Add the correct prop here */}</td>
-            <td>{/** TODO: Add the correct prop here */}</td>
+            <td>{props.factor.description}</td>
+            <td>{'⭐️'.repeat(props.factor.stars)}</td>
+            <td>{props.factor.isPriority ? "YES" : "NO"}</td>
         </tr>
     )
 }
